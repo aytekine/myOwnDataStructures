0fb0b06268d8670bd1929264647c7922af1b4416
aytekine <aaytekinerdogan@gmail.com>
2021-01-04 23:50:27 +0300

    Update UL_Stack.h
    Comments are added!

--- UL_Stack.h
+++ UL_Stack.h
@@ -4,22 +4,22 @@
 class UL_Stack
 {
 private:
-    int m_size;
-    int m_top;
-    int *stack;
+    int m_size; //capacity of the stack
+    int m_top;  //current stack pointer, i.e index of the array
+    int *stack; // pointer to array
 
 public:
-    UL_Stack(int size);
-    ~UL_Stack();
+    UL_Stack(int size); //constructor
+    ~UL_Stack();        //destructor
 
 public:
-    bool isEmpty();
-    bool isFull();
-    int pop();
-    void push(int val);
-    void display();
-    int count();
-    int peek(int pos);
-    void change(int val, int pos);
+    bool isEmpty();     // if 0 stack is not empty, if 1 stack is empty
+    bool isFull();      // if 0 stack is not full , if 1 stack is full
+    int pop();          // Pop the last element from the Stack as in LIFO pattern
+    void push(int val); // Push an element from the Stack as in LIFO pattern
+    void display();     // Display all the elements of the Stack NOT FULL CAPACITY
+    int count();        // Returns the current index of the stack
+    int peek(int pos);  // Returns the <pos>th element of the stack
+    void change(int val, int pos);  // Change the <pos>th element of the stack with <val>
 };
 #endif
